4552395749c928a75625101ae1f64962140c6ff8
Altamash <altamash171201@gmail.com>
2025-06-16 22:11:56 +0530

    class and object basics done

---
+++ rough.py
@@ -0,0 +1,12 @@
+lst=[1,2,3]
+my_str="mlops playlist"
+my_int=155
+
+# print(type(lst))
+# print(type(my_str))
+# print(type(my_int))
+# 
+# lst=lst.capitalize()
+# print(lst)
+# my_str=my_str.capitalize()
+# print(my_str)
